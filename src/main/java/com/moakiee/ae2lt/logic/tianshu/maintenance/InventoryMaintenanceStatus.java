a9185a0175cc70a3ffc26e9ffbb3bd0baf12f1d9
syarukasu <ooshitakiru1014@gmail.com>
2026-08-08 02:48:35 +0900

    port: add 2.0.6 maintenance statuses

---
+++ src/main/java/com/moakiee/ae2lt/logic/tianshu/maintenance/InventoryMaintenanceStatus.java
@@ -0,0 +1,15 @@
+package com.moakiee.ae2lt.logic.tianshu.maintenance;
+
+public enum InventoryMaintenanceStatus {
+    DISABLED,
+    SATISFIED,
+    IDLE,
+    CALCULATING,
+    WAITING_CPU,
+    CRAFTING,
+    MISSING_PATTERN,
+    MISSING_INGREDIENTS,
+    WAITING_RETRY,
+    CANCELLING,
+    OFFLINE
+}
